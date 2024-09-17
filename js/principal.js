375df76d04ef2311ddc6a16eb5e6482969d39295
alkatrauz <163009399+alkatrauz@users.noreply.github.com>
2024-09-17 08:05:54 -0300

    imc

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,8 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paulo = document.querySelector("#paciente1");
+var peso = document.querySelector(".info-peso").textContent;
+var altura = document.querySelector(".info-altura").textContent;
+var imc = peso/(altura*altura);
+paulo.querySelector(".info-imc").textContent = imc;
